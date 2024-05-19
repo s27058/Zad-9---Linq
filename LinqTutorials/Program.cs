206a0a4999913ef8668416d3c10f8a049990313e
s27058 <s27058@pjwstk.edu.pl>
2024-05-19 14:33:09 +0200

    Project

---
+++ LinqTutorials/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = LinqTasks.Task16();
+            foreach (var a in t)
+            {
+                Console.WriteLine(a);
+            }
+            // var y = LinqTasks.Task13(new[] { 1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1 });
+            // Console.WriteLine(y);
+
+        }
+    }
+}
